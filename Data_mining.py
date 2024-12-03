91e5e727eeed7cc5261e61f1a91e03476238ad4a
Panaghs131 <125392587+Panaghs131@users.noreply.github.com>
2024-12-03 22:53:56 +0200

    experimenting

--- Data_mining.py
+++ Data_mining.py
@@ -5,8 +5,10 @@ Created on Mon Dec  2 13:12:48 2024
 @author: panos
 """
 
+
+import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+
 
 pd.set_option('display.max_rows',10)
 pd.set_option('display.max_columns',None)
@@ -19,11 +21,27 @@ df['wip'].fillna(0,inplace = True)
 #a.append(df['actual_productivity']/df['targeted_productivity'])
 df['productivity_ratio'] = df['actual_productivity']/df['targeted_productivity']
 
-df = df.round({'actual_productivity': 4, 'productivity_ratio': 4})
+df = df.round({'actual_productivity': 4, 'productivity_ratio': 4, 'no_of_workers': 0})
+
 df['department'].replace(to_replace = 'sweing',value = 'sewing',inplace = True)
 
-print(df)
+print(df.where(df['wip'] != 0).dropna())
 
 actual_prod_mean = df['actual_productivity'].mean()
 targeted_prod_mean = df['targeted_productivity'].mean()
-print(df[['actual_productivity','over_time']].corr(method='pearson'))
+ratio_prod_mean = df['productivity_ratio'].mean()
+print("Actual prod mean:",actual_prod_mean)
+print("Targeted prod mean:",targeted_prod_mean)
+print("Ratio prod mean:",ratio_prod_mean)
+
+
+correlation_actual_overtime = df[['actual_productivity','over_time']].corr(method='pearson')
+correlation_ratio_overtime = df[['productivity_ratio','over_time']].corr(method='pearson')
+
+df1 = df.where(df['team'] == 1)
+df1.dropna(inplace = True)
+
+plt.plot(df1['productivity_ratio'])
+plt.show()
+
+print(df1['productivity_ratio'].mean())
