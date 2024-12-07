5a1a7447225b746bfeafee461fd038192e0ae532
June <antonisviolentis@gmail.com>
2024-12-07 20:47:30 +0200

    Added dataframe with outliers
    Excluded attributes: ['date', 'quarter', 'day', 'department', 'team', 'actual_productivity', 'no_of_style_change', 'productivity_ratio'] from the dataset. New outlier number: 136.

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,60 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  2 13:12:48 2024
-
-@author: panos
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#Preprocessing
+# ----------------------------------------------------------------- #
+
+# Bloxplots!
+def bloxplot(attribute):
+    # Give attribute as an array made from the df column
+    # Calculate the boxplot
+    median = np.median(df[attribute])
+    q1 = np.quantile(df[attribute], 0.25)
+    q3 = np.quantile(df[attribute], 0.75)
+    iqr = q3 - q1
+    lower_inner_fence = q1 - 1.5 * iqr
+    upper_inner_fence = q3 + 1.5 * iqr
+
+    outlier_df = df[(df[attribute] > upper_inner_fence) | (df[attribute] < lower_inner_fence)]
+    return outlier_df
+
+# Showing graph stuff
+def pretty_graphs(attribute):
+    #This makes the histogram/boxplot graph look good
+    fig, (ax_box, ax_hist) = plt.subplots(
+        nrows=2,
+        sharex=True,  #Share the x-axis
+        gridspec_kw={"height_ratios": [1, 3]}  #Boxplot smaller than histogram
+    )
+    ax_box.boxplot(df[attribute], vert=False, patch_artist=True)
+    ax_box.spines['bottom'].set_visible(False)
+    ax_box.spines['top'].set_visible(False)
+    ax_box.spines['right'].set_visible(False)
+    ax_box.spines['left'].set_visible(False)
+    ax_box.set_yticks([])
+
+    ax_hist.hist(df[attribute], edgecolor='yellow', color='purple')
+    ax_hist.set_ylabel('Frequency')
+    ax_hist.set_xlabel('Values')
+    ax_hist.set_title(f'{attribute}')
+    ax_hist.spines['top'].set_visible(False)
+
+    plt.subplots_adjust(hspace=0)
+    plt.show()
+
+# ----------------------------------------------------------------- #
 
 pd.set_option('display.max_rows',10)
 pd.set_option('display.max_columns',None)
 
-
 df = pd.read_csv(
-    r"""C:/Users/Admin/Desktop/Data-Mining/garments_worker_productivity.csv""")
+    r"""garments_worker_productivity.csv""")
 
-#replacing NaN values with 0
+#Replacing NaN values with 0
 df.fillna({'wip':0},inplace = True)
 
 df['productivity_ratio'] = df['actual_productivity']/df['targeted_productivity']
+
 #Cleaning up attributes
 df = df.round(
     {'actual_productivity': 4, 'productivity_ratio': 4, 'no_of_workers': 0})
@@ -33,6 +66,7 @@ df.replace({'finishing':1},inplace = True)
 
 print(df)
 
+#Calculating mean
 actual_prod_mean = df['actual_productivity'].mean()
 targeted_prod_mean = df['targeted_productivity'].mean()
 ratio_prod_mean = df['productivity_ratio'].mean()
@@ -44,7 +78,9 @@ ratio_prod_mean = df['productivity_ratio'].mean()
 correlation_matrix = []
 corr_df = df[['department','team','no_of_workers','no_of_style_change','targeted_productivity',
               'actual_productivity','smv','wip','over_time','incentive','idle_time','idle_men']]
+outlier_df = pd.DataFrame()
 
+# Correlation plots
 # for i in corr_df.columns:
 #     print()
 #     correlation_actual = df[['actual_productivity',i]].corr(
@@ -57,99 +93,43 @@ corr_df = df[['department','team','no_of_workers','no_of_style_change','targeted
 #     print(correlation_ratio)
 #     print()
 
-# Bloxplots!
-def bloxplot(attribute):
-    #Give attribute as an array made from the df column
-    #Calculate the boxplot
-    median = np.median(attribute)
-    q1 = np.quantile(attribute, 0.25)
-    q3 = np.quantile(attribute, 0.75)
-    iqr = q3-q1
-    lower_inner_fence = q1 - 1.5*iqr
-    upper_inner_fence = q3 + 1.5*iqr
-    
-    return (median, iqr, lower_inner_fence, upper_inner_fence)
-
-def pretty_graphs(attribute):
-    #This makes the histogram/boxplot graph look good
-    fig, (ax_box, ax_hist) = plt.subplots(
-        nrows=2, 
-        sharex=True,  # Share the x-axis
-        gridspec_kw={"height_ratios": [1, 3]}  # Boxplot smaller than histogram
-    )
-    ax_box.boxplot(df[attribute], vert=False, patch_artist=True)
-    ax_box.spines['bottom'].set_visible(False)  
-    ax_box.spines['top'].set_visible(False)  
-    ax_box.spines['right'].set_visible(False)  
-    ax_box.spines['left'].set_visible(False)  
-    ax_box.set_yticks([])  
-    
-    ax_hist.hist(df[attribute], edgecolor='yellow', color='purple')
-    ax_hist.set_ylabel('Frequency')
-    ax_hist.set_xlabel('Values')
-    ax_hist.set_title(f'{attribute}')
-    ax_hist.spines['top'].set_visible(False)  
-
-    plt.subplots_adjust(hspace=0)
-    plt.show()
-    
-
+#Boxplot outlier detection (showing graphs)
 for attr in df:
     if(attr != 'date' and attr != 'quarter' and attr != 'department' and attr != 'day' and attr != 'team'):
-        #Boxplot outlier detection
         attrlist = [x for x in df[attr]]
         attrlist = sorted(attrlist)
         attr_array = np.array(attrlist)
-        # pretty_graphs(attr)
+        #pretty_graphs(attr)
 
-# overtime_box = bloxplot(overtime_array)
-# idle_time_box = bloxplot()
-# idle_men_box = bloxplot()
+#Storing outliers in a new df
+outlier_df = pd.DataFrame()
+att_list = ['date', 'quarter', 'day', 'department', 'team', 'actual_productivity', 'no_of_style_change', 'productivity_ratio']
+aaaaaa = ['idle_time', 'idle_men']
+for attr in df:
+    if (attr not in att_list):
+        outlier_df = pd.concat([outlier_df, bloxplot(attr)])
 
-#df with all the outliers!!!!!
-surprise_tool_that_will_help_us_later = pd.DataFrame()
+outlier_df.dropna(inplace=True)
 
+print("night")
+print(outlier_df)
+print("night")
 
+# ----------------------------------------------------------------- #
 
 mean = np.mean(df['actual_productivity'])
 std = np.std(df['actual_productivity'])
 normal_range=(mean-3*std,mean+3*std)
-print(normal_range)
+#print(normal_range)
 
 minprod = min(df['actual_productivity'])
 maxprod = max(df['actual_productivity'])
-print(minprod,maxprod)
+#print(minprod,maxprod)
 outliers = []
+
 for row in df['actual_productivity']:
     z = abs(row-mean)/std
     if (z > 3):
         outliers.append(row)
         
-print(outliers)
-
-
-
-
-# clean_df = pd.DataFrame(df['over_time'].where((df['over_time'] <= upper) & (
-#                 df['over_time'] >= lower)))
-# clean_df.dropna(inplace = True)
-
-
-# plt.boxplot(clean_df)
-# plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#print(outliers)
