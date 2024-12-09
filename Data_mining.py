0adbeca2b9ad4ab78b5fbab4b092f8cb3e16c04b
FieryInvader <greeksky420@gmail.com>
2024-12-09 20:46:48 +0200

    Fucking around
    Make the averages be the means of actual prod and productivity ratio respectively. fix rules afterwards, if need be

--- Data_mining.py
+++ Data_mining.py
@@ -10,20 +10,20 @@ outliers,clean = pp.run()
 
 #Antecedent/Consequent 
 #Generate array from 0 to a little above productivity ratio max value
-ratio_prod = ctrl.Antecedent(np.arange(0, 2.1, 0.1), 'ratio_prod') 
+productivity_ratio_fuzzy = ctrl.Antecedent(np.arange(0, 2.1, 0.1), 'productivity_ratio_fuzzy') 
 #Generate array from 0 to a little above actual productivity max value
-act_prod = ctrl.Antecedent(np.arange(0, 1.1, 0.1), 'act_prod')
+actual_productivity_fuzzy = ctrl.Antecedent(np.arange(0, 1.1, 0.1), 'actual_productivity_fuzzy')
 
 expectation_concern = ctrl.Consequent(np.arange(0, 11, 1), 'expectation_concern')
 production_concern = ctrl.Consequent(np.arange(0, 11, 1), 'production_concern')
 
-act_prod['good'] = fuzz.trapmf(act_prod.universe,[0.6,0.7,1,1])
-act_prod['average'] = fuzz.trapmf(act_prod.universe, [0.3,0.4,0.6,0.7])
-act_prod['poor'] = fuzz.trapmf(act_prod.universe,[0,0,0.3,0.4])
+actual_productivity_fuzzy['good'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0.6,0.7,1,1])
+actual_productivity_fuzzy['average'] = fuzz.trapmf(actual_productivity_fuzzy.universe, [0.3,0.4,0.6,0.7])
+actual_productivity_fuzzy['poor'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0,0,0.3,0.4])
 
-ratio_prod['good'] = fuzz.trapmf(ratio_prod.universe, [1.1,1.5,2,2])
-ratio_prod['average'] = fuzz.trapmf(ratio_prod.universe, [0.8,1,1.1,1.1])
-ratio_prod['poor'] = fuzz.trapmf(ratio_prod.universe, [0,0,0.6,0.9])
+productivity_ratio_fuzzy['good'] = fuzz.trapmf(productivity_ratio_fuzzy.universe, [1.1,1.5,2,2])
+productivity_ratio_fuzzy['average'] = fuzz.trapmf(productivity_ratio_fuzzy.universe, [0.8,0.9,1.1,1.2])
+productivity_ratio_fuzzy['poor'] = fuzz.trapmf(productivity_ratio_fuzzy.universe, [0,0,0.6,0.9])
 
 # Consequent membership function is formed!
 expectation_concern['low'] = fuzz.trapmf(expectation_concern.universe, [0, 0, 2, 4]) 
@@ -35,41 +35,57 @@ production_concern['low'] = fuzz.trapmf(production_concern.universe, [0, 0, 2, 4
 production_concern['medium'] = fuzz.trapmf(production_concern.universe, [2, 4, 6, 8])
 production_concern['high'] = fuzz.trapmf(production_concern.universe, [6, 8, 10, 10])
 
-ratio_prod.view()
-act_prod.view()
-#concern.view()
+productivity_ratio_fuzzy.view()
+actual_productivity_fuzzy.view()
 
 # Rules
-rule1 = ctrl.Rule(act_prod['poor'] & ratio_prod['poor'], production_concern['high'])
-rule2a = ctrl.Rule(act_prod['poor'] & ratio_prod['average'], production_concern['low'])
-rule2b = ctrl.Rule(act_prod['poor'] & ratio_prod['average'],expectation_concern['low'])
-rule3 = ctrl.Rule(act_prod['average'] & ratio_prod['average'], production_concern['medium'])
-rule4 = ctrl.Rule(act_prod['average'] & ratio_prod['good'], expectation_concern['medium'])
-rule5 = ctrl.Rule(act_prod['good'] & ratio_prod['poor'], expectation_concern['high'])
-rule6 = ctrl.Rule(act_prod['good'] & (ratio_prod['average'] | ratio_prod['good']), production_concern['low'])
+rule1 = ctrl.Rule(actual_productivity_fuzzy['poor'] & productivity_ratio_fuzzy['poor'], production_concern['high'])
+rule2a = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), production_concern['medium'])
+rule2b = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), expectation_concern['low'])
+
+rule3 = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['average'], production_concern['low'])
+rule4 = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['good'], expectation_concern['medium'])
 
 
-#Control System Creation and Simulation
-concern_ctrl = ctrl.ControlSystem([rule1, rule2a, rule2b, rule3, rule4, rule5, rule6])#Create Control System using "ControlSystem" class from the skfuzzy.control module
+rule5 = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['poor'], expectation_concern['high'])
+rule6 = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['average'], expectation_concern['medium'])
+rule7 = ctrl.Rule(actual_productivity_fuzzy['good'], production_concern['low'])
 
-concern_sim = ctrl.ControlSystemSimulation(concern_ctrl)
 
-exp = []
-prod = []
-for index,item in clean[['actual_productivity','productivity_ratio']].iterrows():
-    concern_sim['act_prod'] = item['actual_productivity']
-    concern_sim['ratio_prod'] = item['productivity_ratio']    
-    # Crunch the numbers
-    concern_sim.compute()
+# Control System Creation and Simulation
+concern_ctrl = ctrl.ControlSystem([rule1, rule2a, rule2b, rule3, rule4, rule5, rule6]) # Create Control System using "ControlSystem" class from the skfuzzy.control module
+ 
+simulation = ctrl.ControlSystemSimulation(concern_ctrl)
 
-    production_concern.view(sim=concern_sim)
-    expectation_concern.view(sim=concern_sim)
+expectation_concer_list = []
+production_concer_list = []
+
+simulation.input['actual_productivity_fuzzy'] = 0.5
+simulation.input['productivity_ratio_fuzzy'] = 0.9
+
+simulation.compute()
+
+print(simulation.output)
+production_concern.view(sim=simulation)
+expectation_concern.view(sim=simulation)
+
+# for index,item in clean[['actual_productivity','productivity_ratio']].iterrows():
+#     print(item['actual_productivity'],item['productivity_ratio'] )
+#     simulation.input['actual_productivity_fuzzy'] = item['actual_productivity']
+#     simulation.input['productivity_ratio_fuzzy'] = item['productivity_ratio']    
+#     # Crunch the numbers
+#     simulation.compute()
+
+#     production_concern.view(sim=simulation)
+#     expectation_concern.view(sim=simulation)
     
-    exp.append(concern_sim.output['expectation_concern'])
-    prod.append(concern_sim.output['production_concern'])
+#     print(simulation.output)
     
-    #!! Remove when you are absolutely certain this wont explode !!
-    break
+#     # expectation_concer_list.append(simulation.output['expectation_concern'])
+#     production_concer_list.append(simulation.output['production_concern'])
+    
+#     #!! Remove when you are absolutely certain this wont explode !!
+#     break
 
 
 #print(concern_sim.output)
