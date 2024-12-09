22f6a5f7d9bd9526386ebcfc6bc830143ba17fbb
FieryInvader <greeksky420@gmail.com>
2024-12-09 14:14:47 +0200

    Fuzzy work in progress
    a

--- Data_mining.py
+++ Data_mining.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  2 13:12:48 2024
-
-@author: panos
-"""
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import skfuzzy as fuzz
@@ -15,17 +8,53 @@ import preprocessing as pp
 
 outliers,clean = pp.run()
 
-# New Antecedent/Consequent objects hold universe variables and membership functions
-ratio_prod = ctrl.Antecedent(clean['productivity_ratio'], 'ratio_prod')#Generate the array [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-act_prod = ctrl.Antecedent(clean['actual_productivity'], 'act_prod')
-concern = ctrl.Consequent(np.arange(0, 11, 1), 'concern')
+#Antecedent/Consequent 
+#Generate array from 0 to a little above productivity ratio max value
+ratio_prod = ctrl.Antecedent(np.arange(0, 1.7, 0.1), 'ratio_prod') 
+#Generate array from 0 to a little above actual productivity max value
+act_prod = ctrl.Antecedent(np.arange(0, 1.2, 0.08), 'act_prod')
 
-concern['low'] = fuzz.trapmf(concern.universe, [0, 0, 2, 4]) #edo kathorizo ta akra kai to meso ton trigonon
-concern['medium'] = fuzz.trapmf(concern.universe, [2, 4, 5,7])
-concern['high'] = fuzz.trapmf(concern.universe, [7, 7,10, 10])
+expectation_concern = ctrl.Consequent(np.arange(0, 11, 1), 'expectation_concern')
+production_concern = ctrl.Consequent(np.arange(0, 11, 1), 'production_concern')
 
 act_prod.automf(3)
 ratio_prod.automf(3)
-ratio_prod.view()
-act_prod.view()
-concern.view()
+
+# Consequent membership function is formed!
+expectation_concern['low'] = fuzz.trapmf(expectation_concern.universe, [0, 0, 2, 4]) 
+expectation_concern['medium'] = fuzz.trapmf(expectation_concern.universe, [2, 4, 6, 8])
+expectation_concern['high'] = fuzz.trapmf(expectation_concern.universe, [6, 8, 10, 10])
+
+# Consequent membership function is formed!
+production_concern['low'] = fuzz.trapmf(production_concern.universe, [0, 0, 2, 4]) 
+production_concern['medium'] = fuzz.trapmf(production_concern.universe, [2, 4, 6, 8])
+production_concern['high'] = fuzz.trapmf(production_concern.universe, [6, 8, 10, 10])
+
+# ratio_prod.view()
+# act_prod.view()
+# concern.view()
+
+# Rules
+rule1 = ctrl.Rule(act_prod['poor'] & ratio_prod['poor'], production_concern['high'])
+rule2 = ctrl.Rule(act_prod['poor'] & ratio_prod['average'], production_concern['low'], expectation_concern['low'])
+rule7 = ctrl.Rule(act_prod['poor'] & ratio_prod['average'], expectation_concern['low'])
+rule3 = ctrl.Rule(act_prod['average'] & ratio_prod['average'], production_concern['medium'])
+rule4 = ctrl.Rule(act_prod['average'] & ratio_prod['good'], expectation_concern['medium'])
+rule5 = ctrl.Rule(act_prod['good'] & ratio_prod['poor'], expectation_concern['high'])
+rule6 = ctrl.Rule(act_prod['good'] & (ratio_prod['average'] | ratio_prod['good']), production_concern['low'])
+
+
+#Control System Creation and Simulation
+concern_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7])#Create Control System using "ControlSystem" class from the skfuzzy.control module
+
+concern_sim = ctrl.ControlSystemSimulation(concern_ctrl)
+
+concern_sim.input['act_prod'] = 1
+concern_sim.input['ratio_prod'] = 0.8
+
+# Crunch the numbers
+concern_sim.compute()
+
+print( concern_sim.output['expectation_concern']) 
+production_concern.view(sim=concern_sim)
+expectation_concern.view(sim=concern_sim)
