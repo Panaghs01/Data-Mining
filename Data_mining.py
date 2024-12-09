23088356a3e6bc678b121ee8d37e2ec177cb1da3
Panaghs131 <125392587+Panaghs131@users.noreply.github.com>
2024-12-09 21:29:17 +0200

    Fixed rules, fixed MF, auta

--- Data_mining.py
+++ Data_mining.py
@@ -17,9 +17,9 @@ actual_productivity_fuzzy = ctrl.Antecedent(np.arange(0, 1.1, 0.1), 'actual_prod
 expectation_concern = ctrl.Consequent(np.arange(0, 11, 1), 'expectation_concern')
 production_concern = ctrl.Consequent(np.arange(0, 11, 1), 'production_concern')
 
-actual_productivity_fuzzy['good'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0.6,0.7,1,1])
-actual_productivity_fuzzy['average'] = fuzz.trapmf(actual_productivity_fuzzy.universe, [0.3,0.4,0.6,0.7])
-actual_productivity_fuzzy['poor'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0,0,0.3,0.4])
+actual_productivity_fuzzy['good'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0.8,0.85,1,1])
+actual_productivity_fuzzy['average'] = fuzz.trapmf(actual_productivity_fuzzy.universe, [0.6,0.7,0.8,0.85])
+actual_productivity_fuzzy['poor'] = fuzz.trapmf(actual_productivity_fuzzy.universe,[0,0,0.5,0.7])
 
 productivity_ratio_fuzzy['good'] = fuzz.trapmf(productivity_ratio_fuzzy.universe, [1.1,1.5,2,2])
 productivity_ratio_fuzzy['average'] = fuzz.trapmf(productivity_ratio_fuzzy.universe, [0.8,0.9,1.1,1.2])
@@ -39,35 +39,39 @@ productivity_ratio_fuzzy.view()
 actual_productivity_fuzzy.view()
 
 # Rules
-rule1 = ctrl.Rule(actual_productivity_fuzzy['poor'] & productivity_ratio_fuzzy['poor'], production_concern['high'])
-rule2a = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), production_concern['medium'])
-rule2b = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), expectation_concern['low'])
+rule1a = ctrl.Rule(actual_productivity_fuzzy['poor'] & productivity_ratio_fuzzy['poor'], production_concern['high'])
+rule1b = ctrl.Rule(actual_productivity_fuzzy['poor'] & productivity_ratio_fuzzy['poor'], expectation_concern['low'])
+rule2a = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), production_concern['high'])
+rule2b = ctrl.Rule(actual_productivity_fuzzy['poor'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), expectation_concern['high'])
 
-rule3 = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['average'], production_concern['low'])
-rule4 = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['good'], expectation_concern['medium'])
+rule3a = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['average'], production_concern['low'])
+rule3b = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['average'], expectation_concern['low'])
+rule4a = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['good'], expectation_concern['medium'])
+rule4b = ctrl.Rule(actual_productivity_fuzzy['average'] & productivity_ratio_fuzzy['good'], production_concern['low'])
 
-
-rule5 = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['poor'], expectation_concern['high'])
-rule6 = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['average'], expectation_concern['medium'])
-rule7 = ctrl.Rule(actual_productivity_fuzzy['good'], production_concern['low'])
-
+rule5a = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['poor'], expectation_concern['high'])
+rule5b = ctrl.Rule(actual_productivity_fuzzy['good'] & productivity_ratio_fuzzy['poor'], production_concern['medium'])
+rule6a = ctrl.Rule(actual_productivity_fuzzy['good'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']) , expectation_concern['medium'])
+rule6b = ctrl.Rule(actual_productivity_fuzzy['good'] & (productivity_ratio_fuzzy['average'] | productivity_ratio_fuzzy['good']), production_concern['low'])
+#rule7 = ctrl.Rule(actual_productivity_fuzzy['good'], production_concern['low'])
 
 # Control System Creation and Simulation
-concern_ctrl = ctrl.ControlSystem([rule1, rule2a, rule2b, rule3, rule4, rule5, rule6]) # Create Control System using "ControlSystem" class from the skfuzzy.control module
+concern_ctrl = ctrl.ControlSystem([rule1a, rule1b, rule2a, rule2b, rule3a, rule3b, rule4a, rule4b, rule5a, rule5b, rule6a, rule6b]) # Create Control System using "ControlSystem" class from the skfuzzy.control module
  
 simulation = ctrl.ControlSystemSimulation(concern_ctrl)
 
 expectation_concer_list = []
 production_concer_list = []
 
-simulation.input['actual_productivity_fuzzy'] = 0.5
-simulation.input['productivity_ratio_fuzzy'] = 0.9
+simulation.input['actual_productivity_fuzzy'] = 0.8
+simulation.input['productivity_ratio_fuzzy'] = 0.5
 
 simulation.compute()
 
-print(simulation.output)
+
 production_concern.view(sim=simulation)
 expectation_concern.view(sim=simulation)
+print(simulation.output)
 
 # for index,item in clean[['actual_productivity','productivity_ratio']].iterrows():
 #     print(item['actual_productivity'],item['productivity_ratio'] )
